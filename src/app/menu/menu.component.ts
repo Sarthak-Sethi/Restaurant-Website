d3ce7f81df35c5ec2353214b24265cb6260fff7e
Sarthak-Sethi <sarthaksethigithub@gmail.com>
2021-08-22 14:31:22 +0530

    3 ways to return 1.promise,2.bservable to promise,3.Direct observable

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -17,12 +17,18 @@ export class MenuComponent implements OnInit {
   selectedDish: Dish = DISHES[0];
 
   
+// When A promise is returned
+  // ngOnInit(): void {
+  //   this.dishService.getDishes().then(
+  //     (dishes) => {this.dishes = dishes;}    ) ;
+  // }
 
+// When A observable is returend 
   ngOnInit(): void {
-    this.dishService.getDishes().then(
-      (dishes) => {this.dishes = dishes;}    ) ;
+    this.dishService.getDishes().subscribe(
+      (dishes) => {this.dishes = dishes;}
+      );
   }
-
   showDescription(dish: Dish) {
     this.selectedDish = dish;
 
--- src/app/services/dishservice.service.ts
+++ src/app/services/dishservice.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../model/dish';
 import { DISHES } from '../data/dishes';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -8,13 +11,27 @@ import { DISHES } from '../data/dishes';
 export class DishserviceService {
 dishes : Dish[] = DISHES;
 
+// Returning a promise 
 
-  getDishes() : Promise<Dish []> {
-    return new Promise(
-      (resolve,reject) => {
-        resolve(this.dishes);
-      }
-      )
-  }
+  // getDishes() : Promise<Dish []> {
+  //   return new Promise(
+  //     (resolve,reject) => {
+  //       resolve(this.dishes);
+  //     }
+  //     )
+  // }
+
+  // Returning A oberrvable  and rhen converting it to promise
+  // getDishes() : Promise<Dish []> {
+  // return of(DISHES).pipe(delay(2000)).toPromise();
+  // }
+
+// Returning a Observable Directly
+getDishes(): Observable<Dish[]> {
+  return of(DISHES).pipe(delay(2000));
+}
+
+
+
   constructor() { }
 }
